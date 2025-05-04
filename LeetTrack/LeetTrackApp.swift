31fa92d6dd20ee62379cf98e428ae6557bb1e00b
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-05-04 09:19:39 +0530

    Initial Commit

---
+++ LeetTrack/LeetTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetTrackApp.swift
+//  LeetTrack
+//
+//  Created by Simarjeet Kaur on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LeetTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
